ccfffa910cd55d4a8d65a8818059a157c4a0c91f
Michael Nizzari <michael.nizzari@gmail.com>
2017-10-26 10:33:42 -0400

    010 - Overflow and Underflow

---
+++ 010-overflow-underflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_overflow_underflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 255;
+            Console.WriteLine("a = " + a);
+            a += 1;
+            Console.WriteLine("a += 1 = " + a);
+
+            byte b = 0;
+            Console.WriteLine("b = " + b);
+            b -= 1;
+            Console.WriteLine("b -= 1 = " + b);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 010-overflow-underflow/readme.md
@@ -0,0 +1,4 @@
+﻿# Overflow and Underflow
+
+Demonstrates the wrap-around effect when a numeric
+primitive exceeds its maximum or minimum value.
