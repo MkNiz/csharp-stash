7fca957f1e52ff503699fc05ea9bf88a1ba8f53b
Michael Nizzari <michael.nizzari@gmail.com>
2017-10-27 10:38:08 -0400

    019 - For Loop and Break

---
+++ 019-for-loop-and-break/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _019_for_loop_and_break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iterating:");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("i: " + i);
+            }
+
+            Console.WriteLine("Finding first number divisible by 5:");
+            for (int j = 1; j < 999999999; j++)
+            {
+                if (j % 5 == 0)
+                {
+                    Console.WriteLine(j + " is divisible by 5.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(j + " is not divisible by 5.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 019-for-loop-and-break/readme.md
@@ -0,0 +1,4 @@
+﻿# For Loop and Break
+
+Demonstrates using a for loop to iterate on a value,
+as well as using the break keyword to terminate a loop.
