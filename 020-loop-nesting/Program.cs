536d0c0e4b931648b4b3d67e71943bca03a0fd13
Michael Nizzari <michael.nizzari@gmail.com>
2017-10-30 09:56:00 -0400

    020 - Loop Nesting

---
+++ 020-loop-nesting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _020_loop_nesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if ((10*i + j) % 2 == 0)
+                    {
+                        Console.Write("x");
+                    }
+                    else
+                    {
+                        Console.Write("o");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 020-loop-nesting/readme.md
@@ -0,0 +1,3 @@
+﻿# Loop Nesting
+
+Demonstrates nesting a for loop to perform actions in "2 dimensions"
