43d9952355401f5871bd6112ed609d3393f964da
Michael Nizzari <michael.nizzari@gmail.com>
2017-10-31 10:56:46 -0400

    024 - Enumerations

---
+++ 024-enumerations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _024_enumerations
+{
+    class Program
+    {
+        public enum PlayerClass { FightingMan, MagicUser, Priest };
+
+        static void Main(string[] args)
+        {
+            PlayerClass bobClass = PlayerClass.FightingMan;
+
+            switch (bobClass)
+            {
+                case PlayerClass.FightingMan:
+                    Console.WriteLine("Bob is a Fighting Man.");
+                    break;
+                case PlayerClass.MagicUser:
+                    Console.WriteLine("Bob is a Magic User.");
+                    break;
+                case PlayerClass.Priest:
+                    Console.WriteLine("Bob is a Priest.");
+                    break;
+                default:
+                    Console.WriteLine("Bob does not have a class.");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 024-enumerations/readme.md
@@ -0,0 +1,4 @@
+﻿# Enumerations
+
+Demonstrates using enumerations as a lightweight means of defining a constraint
+of specific possible values for a variable.
