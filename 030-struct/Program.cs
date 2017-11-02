896ca0cf45b7a02de76b4fcb29fbcb2b3dada74c
Michael Nizzari <michael.nizzari@gmail.com>
2017-11-02 15:35:36 -0400

    030 - Structure

---
+++ 030-struct/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _030_struct
+{
+    struct MyTime
+    {
+        private int seconds;
+
+        public int Seconds
+        {
+            get
+            {
+                return seconds;
+            }
+            set
+            {
+                Console.WriteLine("Now contains " + value + " seconds.");
+                seconds = value;
+            }
+        }
+
+        public int Minutes
+        {
+            get
+            {
+                return seconds / 60;
+            }
+            set
+            {
+                Console.WriteLine("Now contains " + value + " minutes.");
+                seconds = value * 60;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTime t = new MyTime();
+
+            t.Seconds = 500;
+            t.Minutes = 5;
+            Console.WriteLine("That's " + t.Seconds + " seconds.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 030-struct/readme.md
@@ -0,0 +1,8 @@
+﻿# Structure
+
+Demonstrates a structure (struct) in C#. This behaves similarly to a class in some
+ways as it has instance variables and methods, but has significant differences.
+
+A struct cannot have a parameterless constructor, and it resides on the stack instead of the heap.
+
+They are also value types, meaning a full object gets passed around instead of a reference.
