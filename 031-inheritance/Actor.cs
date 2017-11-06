60408eaea638e35f0a271fdf6f094569d37da3d7
Michael Nizzari <michael.nizzari@gmail.com>
2017-11-06 10:24:05 -0500

    031 - Inheritance - Base Class

---
+++ 031-inheritance/Actor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _031_inheritance
+{
+    class Actor
+    {
+        protected string name { get; set; }
+
+        protected int hp;
+        protected int atk;
+        protected int def;
+
+        public Actor(string name, int hp, int atk, int def)
+        {
+            this.name = name;
+            this.hp = hp;
+            this.atk = atk;
+            this.def = def;
+        }
+
+        public void attack(Actor target)
+        {
+            Console.WriteLine(name + " attacks " + target.name + "!");
+            target.takeHit(atk);
+        }
+
+        public void takeHit(int incomingDmg)
+        {
+            int mitigatedDmg = incomingDmg - (def / 4);
+            if (mitigatedDmg < 1) { mitigatedDmg = 1; }
+            Console.WriteLine(name + " took " + mitigatedDmg + " damage! ");
+            hp -= mitigatedDmg;
+        }
+
+        public void battleCry()
+        {
+            Console.WriteLine(name + " screams!");
+        }
+    }
+}
