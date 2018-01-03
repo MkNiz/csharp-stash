02f89cc783a982fec2b1bbeea23a8e0cda3081e9
Michael Nizzari <michael.nizzari@gmail.com>
2018-01-03 09:44:26 -0500

    037 - File - Write

---
+++ 037-file-write/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _037_file_write
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("hello.txt", "Hello World!");
+            Console.WriteLine("Wrote hello.txt");
+
+            string[] lines = new string[3];
+            lines[0] = "hello";
+            lines[1] = "HeLlo";
+            lines[2] = "HELLO!";
+            File.WriteAllLines("hello2.txt", lines);
+            Console.WriteLine("Wrote hello2.txt");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 037-file-write/readme.md
@@ -0,0 +1,3 @@
+﻿# File - Write
+
+Demonstrates writing simple text files, both from a string and from a string array representing each line
