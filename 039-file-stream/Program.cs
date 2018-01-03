6d40609a66e2255aa7d242a98d5cbb6a64e1cf59
Michael Nizzari <michael.nizzari@gmail.com>
2018-01-03 10:06:31 -0500

    039 - File - Stream

---
+++ 039-file-stream/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _039_file_stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Streaming output
+            FileStream myStream = File.OpenWrite("output.txt");
+            StreamWriter sw = new StreamWriter(myStream);
+
+            sw.Write("Henlo");
+            sw.Write("fff\n");
+            sw.Write("cheas");
+
+            sw.Flush();
+            sw.Close();
+
+            Console.WriteLine("Wrote output.txt.");
+
+            // Streaming input
+            Console.WriteLine("Reading input.txt:\n");
+
+            myStream = File.OpenRead("../../input.txt");
+            StreamReader sr = new StreamReader(myStream);
+
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+
+            sr.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 039-file-stream/readme.md
@@ -0,0 +1,3 @@
+﻿# File - Stream
+
+Demonstrates writing and reading files using a stream, allowing only relevant data to be stored at a given time
