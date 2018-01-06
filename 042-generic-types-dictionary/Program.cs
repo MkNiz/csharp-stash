7fee1f491040f78bcd615a654ae160e05ac0cada
Michael Nizzari <michael.nizzari@gmail.com>
2018-01-06 10:22:04 -0500

    042 - Generic Types: Dictionary

---
+++ 042-generic-types-dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _042_generic_types_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> powerLevels = new Dictionary<string, int>();
+            powerLevels["Gofu"] = 9001;
+            powerLevels["Pant"] = 120;
+            powerLevels["Steve"] = 0;
+
+            foreach (KeyValuePair<string, int> item in powerLevels) {
+                Console.WriteLine(item.Key + "'s power level is " + item.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 042-generic-types-dictionary/readme.md
@@ -0,0 +1,3 @@
+﻿# Generic Types: Dictionary
+
+Demonstrates using a dictionary, which allows one to look up a value in the data structure by specifying a key
