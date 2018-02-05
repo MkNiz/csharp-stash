ccaa7032d5a5cea470a9e014cd5357690bd42b00
Michael Nizzari <michael.nizzari@gmail.com>
2018-02-04 19:50:01 -0500

    055 - XmlReader

---
+++ 055-xml-reader/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _055_xml_reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int invCounter = 1;
+
+            Console.WriteLine("Inventory:");
+
+            // Create the XmlReader and direct it to the inventory.xml file
+            XmlReader reader = XmlReader.Create("inventory.xml");
+
+            // Run loop while file is still being read
+            while (reader.Read())
+            {
+                // When reading encounters an element with the element name item...
+                if((reader.NodeType == XmlNodeType.Element) && (reader.Name == "item"))
+                {
+                    // Print the item's name and description (while incrementing the invCounter variable)
+                    Console.WriteLine("\t" + invCounter++ + ".) " + reader.GetAttribute("name") +
+                        " - " + reader.GetAttribute("description"));
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 055-xml-reader/readme.md
@@ -0,0 +1,5 @@
+﻿# XmlReader
+
+Demonstrates creating and using an XmlReader to read from an XML file in sequence.
+
+This only loads a small part of the XML file at a time, which can be useful.
