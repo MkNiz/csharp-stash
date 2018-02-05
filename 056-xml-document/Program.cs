58d7eb9ddb380094945742dd7ca3b0fb560aeb83
Michael Nizzari <michael.nizzari@gmail.com>
2018-02-05 09:28:18 -0500

    056 - XmlDocument

---
+++ 056-xml-document/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _056_xml_document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an XmlDocument and use it to load items.xml
+            XmlDocument doc = new XmlDocument();
+            doc.Load("items.xml");
+            
+            // Create an XmlNodeList to allow iteration through every item that is a weapon element
+            XmlNodeList weapons = doc.SelectNodes("/items/weapon");
+
+            // Iterate through and output the results
+            Console.WriteLine("Weapons:");
+            foreach (XmlNode node in weapons)
+                Console.WriteLine("\t- " + node.Attributes["name"].Value + ": " + node.Attributes["description"].Value);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 056-xml-document/readme.md
@@ -0,0 +1,4 @@
+﻿# XmlDocument
+
+Demonstrates using XmlDocument to load an XML file and grab specific element tags to
+be iterated on
