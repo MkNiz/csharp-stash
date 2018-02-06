d6a7bcef3e8b98b3dbf7b5a0144d7658c372e9a3
Michael Nizzari <michael.nizzari@gmail.com>
2018-02-06 14:06:34 -0500

    064 - Querying 2

---
+++ 064-querying-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _064_querying_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<OrganDonor> donors = new List<OrganDonor>
+            {
+                new OrganDonor("Steve", 2),
+                new OrganDonor("Julie", 0),
+                new OrganDonor("Postulio", 1),
+                new OrganDonor("Matilda", 2),
+                new OrganDonor("Oedbaed", 0)
+            };
+
+            Predicate<OrganDonor> testKidneys = delegate (OrganDonor od) { return od.Kidneys > 0; };
+            List<OrganDonor> donorsWithKidneys = donors.FindAll(testKidneys);
+
+            Console.WriteLine("Donors with kidneys available:");
+            Action<OrganDonor> output = Console.WriteLine;
+            donorsWithKidneys.ForEach(output);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 064-querying-2/readme.md
@@ -0,0 +1,3 @@
+﻿# Querying 2
+
+Demonstrates the C#2 means of querying a data structure by using a predicate and action to separate the process into both sorting and printing
